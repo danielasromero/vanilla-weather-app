865e3c9a2919db5a9ea21919b07518f6aba4f261
danielasromero <98064811+danielasromero@users.noreply.github.com>
2022-04-10 16:58:17 -0600

    Got rid of Fahrenheit conversion, changed font size on current temp

--- src/app.js
+++ src/app.js
@@ -25,7 +25,6 @@ function formatDate(timestamp) {
         ];
 
     return days[day];
-
 }
 
 function displayForecast(response) {
@@ -69,6 +68,7 @@ function search(city) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
     axios.get(apiUrl).then(showTemp);
 }
+
 function changeCity(event) {
     event.preventDefault();
     let heading = document.querySelector("#city");
@@ -97,16 +97,6 @@ function showTemp(response) {
     let tempElement = document.querySelector("#temp-value");
     tempElement.innerHTML = temperature_value;
 
-    // Removed high - low for now - fully removed from html 
-
-    /*let temp_high = Math.round(response.data.main.temp_max);
-    let tempHighElement = document.querySelector("#high-temp");
-    tempHighElement.innerHTML = `H: ${temp_high} °C`;
-
-    let temp_low = Math.round(response.data.main.temp_min);
-    let tempLowElement = document.querySelector("#low-temp");
-    tempLowElement.innerHTML = `L: ${temp_low} °C`; */
-
     let wind_speed = Math.round(response.data.wind.speed);
     let windSpeedElement = document.querySelector("#wind-speed");
     windSpeedElement.innerHTML = `Wind Speed: ${wind_speed} km/h`
@@ -124,43 +114,5 @@ function showTemp(response) {
 let citySearch = document.querySelector("form");
 citySearch.addEventListener("submit", changeCity);
 
-/* function getCurrentTemp(position) {
-  let lat = position.coords.latitude;
-  let long = position.coords.longitude;
-  let apiKey = "35a610a7ee1e91c0a989468ef9ec0328";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
-  axios.get(apiUrl).then(showTemp);
-} 
-
-function getCurrentLocation(event) {
-    navigator.geolocation.getCurrentPosition(getCurrentTemp);
-}
-
-let currentCity = document.querySelector("#current-location-button")
-currentCity.addEventListener("click", getCurrentLocation);*/
-
-// Bonus: Celcius to Farenheit conversion
-let temp = document.querySelector("#temp-value");
-
-function tempToCelcius(event){
-    event.preventDefault();
-    farLink.classList.remove("active");
-    celciusLink.classList.add("active");
-    temp.innerHTML = Math.round(celciusTemp);
-}
-function tempToFahrenheit(event){
-    event.preventDefault();
-    celciusLink.classList.remove("active");
-    farLink.classList.add("active");
-    let fahrenheitTemp = (celciusTemp * 9) / 5 + 32;
-    temp.innerHTML = Math.round(fahrenheitTemp);
-}
-let celciusLink = document.querySelector("#celcius-link");
-celciusLink.addEventListener("click", tempToCelcius);
-
-let farLink = document.querySelector("#fahrenheit-link");
-farLink.addEventListener("click",tempToFahrenheit); 
-
-let celciusTemp = null;
 
 search("Sydney");
